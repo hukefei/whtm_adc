e990023eeed54bd3588c9668d61bbd14807214cd
huker <hukeroy@163.com>
2019-09-25 14:25:09 +0800

    hotfix

--- classification/gt2cls.py
+++ classification/gt2cls.py
@@ -3,6 +3,29 @@ import pandas as pd
 import numpy as np
 import os
 
+class code_dict():
+    def __init__(self, code_file):
+        self.code_df = pd.read_excel(code_file)
+        assert 'CODE' in self.code_df.columns, 'CODE should be in the code file'
+        assert 'ID' in self.code_df.columns, 'ID should be in the code file'
+
+    def code2id(self, code_lst):
+        if isinstance(code_lst, str):
+            return self.code_df.loc[self.code_df['CODE'] == code_lst, 'ID'].values[0]
+        if isinstance(code_lst, list):
+            id_lst = []
+            for code in code_lst:
+                id_lst.append(self.code_df.loc[self.code_df['CODE'] == code, 'ID'].values[0])
+            return id_lst
+
+    def id2code(self, id_lst):
+        if isinstance(id_lst, int):
+            return self.code_df.loc[self.code_df['ID'] == id_lst, 'CODE'].values[0]
+        if isinstance(id_lst, list):
+            code_lst = []
+            for id in id_lst:
+                code_lst.append(self.code_df.loc[self.code_df['ID'] == id, 'CODE'].values[0])
+            return code_lst
 
 def prio_check(prio_lst, code_lst):
     idx_lst = []
@@ -17,6 +40,7 @@ def prio_check(prio_lst, code_lst):
 def gt2cls(json_file,
            test_images,
            prio_file,
+           code_dict_obj,
            output='ground_truth_result.xlsx'):
     with open(json_file) as f:
         json_dict = json.load(f)
@@ -42,6 +66,7 @@ def gt2cls(json_file,
         assert len(annos_per_img) != 0, 'no gt bbox in image {}'.format(img)
 
         category_lst = list(annos_per_img['category_id'].values)
+        category_lst = code_dict_obj.id2code(category_lst)
         gt_code = prio_check(prio_lst, category_lst)
         gt_result.append({'image name': img, 'true code': gt_code})
         json_img_lst.append(img)
@@ -57,7 +82,9 @@ def gt2cls(json_file,
 
 
 if __name__ == '__main__':
-    gt_json = r'D:\Project\WHTM\data\21101\train_test_data'
-    prio_file = r'D:\Project\WHTM\data\21101\train_test_data'
-    test_images = r''
-    gt2cls(gt_json, test_images, prio_file)
+    gt_json = r'D:\Project\WHTM\data\21101\train_test_data\test.json'
+    prio_file = r'D:\Project\WHTM\data\21101\21101_prio.xlsx'
+    test_images = r'D:\Project\WHTM\data\21101\train_test_data\test'
+    code_file = r'D:\Project\WHTM\data\21101\21101_code_dict.xlsx'
+    code = code_dict(code_file)
+    gt2cls(gt_json, test_images, prio_file, code)
